a3c6ae213744ddf52aed8885df670ab6990f7eb2
eureu <venekbespalov@mail.ru>
2022-12-16 01:56:05 +0300

    '

--- animestock/animestock/settings.py
+++ animestock/animestock/settings.py
@@ -135,6 +135,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+#MEDIA
+MEDIA_ROOT = os.path.join(BASE_DIR, 'anime_posters')
+MEDIA_URL = '/anime_posters/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
