060880b50b22365e93f27c086e69b4bca4c5f5c7
eureu <venekbespalov@mail.ru>
2022-12-16 17:35:47 +0300

    '

--- animestock/main_site/views.py
+++ animestock/main_site/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
+from .models import Anime
 # Create your views here.
 
 def index(request):
+
+    search_query = request.GET.get("q")
+    if search_query:
+        pass
+
     return render(request, 'index.html')
+
+
+# def genres(request):
+#     return render(request, genres.html)
+
+
+class Search(ListView):
+
+    # template_name = 
+    def get_queryset(self):
+        return Anime.objects.filter(title__icontains=self.request.GET.get("q"))
+
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["q"] = self.request.GET.get("q")
+        return context
+
+    # return render(request=request, template_name=self.template_name, context=context)
+# https://evileg.com/ru/post/364/
