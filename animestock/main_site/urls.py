69f50f61e8881940f0a4cba7c6447ecb3eab8317
eureu <venekbespalov@mail.ru>
2023-01-29 15:51:08 +0300

    '

--- animestock/main_site/urls.py
+++ animestock/main_site/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     path('search/', views.Search.as_view(), name='search'),
     path('genres/', views.genres, name='genres'),
     path('anime/', views.anime, name='anime'),
-    url(r'^genres/<slug:slug>/<int:_id>', views.AnimeDetail, name='page_anime'),
+    path('genres/<slug:slug>', views.AnimeDetail.as_view(), name='page_anime'),
     path('register', views.registerPage, name='register'),
     path('login', views.loginPage, name='login'),
     path('logout', views.logoutUser, name='logout'),
     path('favourites', views.add_to_favourites, name='favourites'),
 ]
 #  genres/<slug:slug>/
+# url(r'^genres/<slug:slug>/<int:_id>', views.AnimeDetail, name='page_anime'),
